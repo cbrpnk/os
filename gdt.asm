ae9abecb9124d2434c0745e62eff681d1bab8e84
cbrpnk <emmanuelturpin@gmail.com>
2021-05-21 16:15:31 -0400

    Added a global descriptor table

---
+++ gdt.asm
@@ -0,0 +1,28 @@
+gdt:
+gdt_null:
+	dd 0x0
+	dd 0x0
+
+gdt_code:
+	dw 0xffff		; Segment limit 0-15
+	dw 0x0000		; Base address 0-15
+	db 0x00			; Base address 16-23
+	db 10011010b	; Code segment, non-system, ring 0, Present in memory
+	db 11001111b	; Last nibble of segment limit + Flags << 4
+	db 0x00			; Base address 31-24
+
+gdt_data:
+	dw 0xffff		; Segment limit 0-15
+	dw 0x0000		; Base address 0-15
+	db 0x00			; Base address 16-23
+	db 10010010b	; Data segment, non-system, ring 0, Present in memory
+	db 11001111b	; Last nibble of segment limit + Flags << 4
+	db 0x00			; Base address 31-24
+	
+gdt_descriptor:
+	dw 24-1			; Size - 1 (Why -1, idk)
+	dd gdt			; Address
+
+; Segment offsets used to populate the segment registers
+CODE_SEGMENT equ gdt_code - gdt
+DATA_SEGMENT equ gdt_data - gdt
--- main.asm
+++ main.asm
@@ -1,35 +1,43 @@
 [org 0x7c00]
 
 ; Setup stack
-mov bp, 0x8000
+mov bp, 0x9000
 mov sp, bp
 
-;mov bx, str
-;mov ax, 5
-;call print_hex_buf
-
-mov bx, str
+mov bx, intro_msg
 call print
 
-mov bx, 0x9000
-call disk_read
-mov ax, 512
-call print_hex_buf
+;mov bx, 0x9000
+;call disk_read
+;mov ax, 512
+;call print_hex_buf
+
+; Switch to 32bit protected mode
+cli						; Clear interupts
+lgdt [gdt_descriptor]	; Set the global descriptor table
+mov eax, cr0
+or	eax, 0x1			; Set the control bit
+mov cr0, eax
+
+; TODO We have to jump far away to flush the cpu pipeline
 
 jmp $ ; infinite loop
 
 ; Includes
 %include "print.asm"
 %include "disk.asm"
+%include "gdt.asm"
 
-str:
+intro_msg:
 	db "Manu Boot Loader", 0x0a, 0
 
 ; Padding + Magic number
 times 510-($-$$) db 0
 dw 0xaa55
 
-; Second sector
+
+; ---------------- Second sector starts here ------------------
+
 db 0x11
 db 0x22
 db 0x33
